430363ad31a55595eeb41599f4f158e05d0eb9b0
NolanPeterson453 <79056951+NolanPeterson453@users.noreply.github.com>
2023-12-03 20:42:10 -0600

    previewing markdown on github

--- Scripts/eda.R
+++ Scripts/eda.R
@@ -1,6 +1,18 @@
 source(file = "./Scripts/data_cleaning.R")
 source(file = "./Scripts/helper_functions.R")
 
+# Look at summary statistics for Obesity rate in adults by income level 
+cln_tran_cdc %>% 
+  expand_by_weight("Sample_Size") %>% 
+  group_by(income_strat) %>% 
+  summarise(min_obs_rate = min(Data_Value, na.rm = TRUE),
+            max_obs_rate = max(Data_Value, na.rm = TRUE),
+            avg_obs_rate = mean(Data_Value, na.rm = TRUE),
+            median_obs_rate = median(Data_Value, na.rm = TRUE))
+
+cln_tran_cdc %>% ggplot2::ggplot(aes(y = Data_Value))
+
+
 # use function to return estimates of marginal effects from moving up an income level on probability of being obese
 marginal_effects <- 
   marginal_ods_calc(y = cln_tran_cdc$Data_Value/100,
@@ -12,7 +24,9 @@ marginal_effects |> ggplot(aes(y =  marginal_or,
   geom_point() +
   geom_errorbar(aes(ymin=CI_lower, ymax=CI_upper), width=.2,
                 position=position_dodge(0.05)) +
-  labs(title = "Change in Odds Ratio")
+  labs(title = "Change in Odds Ratio",
+       y = "Marginal Odd Ratio",
+       x = "Income Level")
 
 
 
